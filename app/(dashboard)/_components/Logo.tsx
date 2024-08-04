f56f039fa04662a5c68b90ab404c6f360004a321
offor <offorchukwuebuka8@gmail.com>
2024-08-04 07:15:34 +0200

    done

--- app/(dashboard)/_components/Logo.tsx
+++ app/(dashboard)/_components/Logo.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 
 export const Logo = () => {
   return (
+    <Link href="/">
     <div className='flex items-center'>
         <Image src="/logo.svg" height={30} width={30} alt='Logo' />
         <span className='ml-4 text-[#0369a1] font-semibold'>LMS</span>
     </div>
+    </Link>
   )
 }
