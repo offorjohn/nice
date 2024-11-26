ff2cea1f9f24b7061c3b0db0889e2434e26e7824
offor <offorchukwuebuka3@gmail.com>
2024-11-26 19:10:48 +0100

    added dark and wite mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
       <ClerkProvider>
         <html lang="en">
           <body className={inter.className}>
-          <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+          <ThemeProvider attribute="class">
           <Theme appearance="dark">
             <ConfettiProvider/>
             <ToasterProvider/>
