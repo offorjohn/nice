14e16fa72888adb3625365d58a9f84ca29e68397
offor <offorchukwuebuka3@gmail.com>
2024-11-26 19:25:06 +0100

    added dark and wite mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 // app/layout.tsx
 
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
@@ -8,14 +7,9 @@ import { ToasterProvider } from '@/components/providers/toaster-provider'
 import { ConfettiProvider } from '@/components/providers/ConfettiProvider'
 import { ThemeProvider } from 'next-themes'
 import { Theme } from "@radix-ui/themes"
+import { metadata } from './metadata' // Import metadata here
 
 const inter = Inter({ subsets: ['latin'] })
-
-// Server-side metadata export (no client-side dependencies here)
-export const metadata: Metadata = {
-  title: 'LMS-Platform',
-  description: 'Courses For Free',
-}
 
 // RootLayout component (client-side logic inside)
 export default function RootLayout({
---
+++ app/metadata.ts
@@ -0,0 +1,8 @@
+// app/metadata.ts
+
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'LMS-Platform',
+  description: 'Courses For Free',
+}
