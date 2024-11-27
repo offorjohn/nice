73a7b7fb9f4767b1ab715754cba66aec61d8e1cc
offor <offorchukwuebuka3@gmail.com>
2024-11-27 04:16:51 +0100

    made sidebar a litle better

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider afterSignUpUrl="/">
+      <ClerkProvider>
         <html lang="en">
           <body className={inter.className}>
           <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
